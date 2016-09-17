14aa0861f8ffbc34abab1d3543d939d484d8382f
josue <jc.derivado.integral@gmail.com>
2016-09-17 12:44:59 -0500

    Classes

---
+++ codeJam2008Round1A/src/codeJam2008Round1A/Milkshakes.java
@@ -0,0 +1,61 @@
+package codeJam2008Round1A;
+
+import java.util.Scanner;
+import java.util.Hashtable;
+
+public class Milkshakes {
+
+	@SuppressWarnings("resource")
+	public static void main(String[]args){
+		Scanner in = new Scanner(System.in);
+		int testCases = in.nextInt();
+		for(int i=0;i<testCases;i++){
+			Hashtable<Integer,Integer> saboresDisponibles = new Hashtable<Integer,Integer>(); 
+			int sabores = in.nextInt();
+			for(int j=1; j<=sabores; j++){
+				saboresDisponibles.put(j, new Integer(-1));
+			}
+			int clientes = in.nextInt();
+			boolean siPudo = false;
+			
+			for(int j=0; j<clientes;j++){
+				int sabCliente = in.nextInt();				
+				for(int k=0; k<sabCliente;k++){
+					int key = in.nextInt();
+					int tipo = in.nextInt();
+					if(saboresDisponibles.get(key) == -1){
+						saboresDisponibles.put(key, tipo);
+						siPudo = true;
+					}
+					else {
+						if(saboresDisponibles.get(key) == tipo)
+							siPudo = true;
+						else{
+							if(k+1 == sabCliente)
+								siPudo = false;
+						}
+					}
+				}				
+			}
+			
+			//Revisar la hashtable para imprimir los valores
+			if(siPudo){
+				int[] salida = new int[sabores];
+				for(Integer j: saboresDisponibles.keySet()){
+					if(saboresDisponibles.get(j) == -1)
+						saboresDisponibles.put(j, 0);
+					salida[j-1] = saboresDisponibles.get(j);
+					
+				}
+				System.out.print("Case #"+(i+1)+": ");
+				for(int k=0; k<salida.length;k++)
+					System.out.print(salida[k]+" ");
+				System.out.println();
+			}
+			else{
+				System.out.println("Case #"+(i+1)+": "+"IMPOSSIBLE");
+			}
+			
+		}
+	}
+}
---
+++ codeJam2008Round1A/src/codeJam2008Round1A/MinimumScalarProduct.java
@@ -0,0 +1,35 @@
+package codeJam2008Round1A;
+
+import java.util.*;
+import java.math.*;
+
+public class MinimumScalarProduct {
+
+	@SuppressWarnings("resource")
+	public static void main(String[]args){
+		Scanner in = new Scanner(System.in);
+		int testCases = in.nextInt();
+		PriorityQueue<BigInteger> minHeap = new PriorityQueue<BigInteger>();
+		for(int i=0;i<testCases;i++){
+			int m = in.nextInt();
+			BigInteger val = BigInteger.valueOf(0);
+			PriorityQueue<BigInteger> maxHeap = new PriorityQueue<BigInteger>(m,Collections.reverseOrder());
+			for(int j=0;j<m;j++){
+				BigInteger b = BigInteger.valueOf(in.nextInt()); 
+				minHeap.offer(b);
+			}
+			for(int j=0;j<m;j++){
+				BigInteger b = BigInteger.valueOf(in.nextInt()); 
+				maxHeap.offer(b);
+			}
+			for(int j=0;j<m;j++){
+				//System.out.println("MULT: "+minHeap.peek()+","+maxHeap.peek()+" ->: "+minHeap.peek().multiply(maxHeap.peek()) );
+				val = val.add(minHeap.poll().multiply(maxHeap.poll()));
+				//System.out.println("VAL: "+ val );
+			}
+			System.out.println("Case #"+(i+1)+": "+val);
+			
+		}
+		
+	}
+}
